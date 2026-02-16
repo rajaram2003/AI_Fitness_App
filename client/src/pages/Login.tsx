ffa0afe870c3264b03c927677ac82e65dad834d2
rajaram2003 <rajarambiswal123@gmail.com>
2026-02-16 19:15:47 +0530

    add

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,36 @@
+import { AtSignIcon } from "lucide-react"
+import { useState } from "react"
 
 const Login = () => {
+
+  const [state, setState] = useState('sign up')
   return (
-    <div>Login</div>
+    <>
+    <main className="login-page-container">
+      <form className="login-form">
+        <h2 className="text-3xl font-medium text-gray-900 dark:text-white">
+          {state === 'login' ? "Sign In" : "Sign up"}
+        </h2>
+        <p className="mt-2 text-sm text-gray-500/90 dark:text-gray-400">
+          {state === "login"
+          ? "Welcome back! Sign in to continue your fitness journey."
+          : "Create your account and start your fitness journey today."}
+        </p>
+
+        {/* Username */}
+
+        {state !== 'login' && (
+          <div className="mt-4">
+            <label className="font-medium text-sm text-gray-700 dark:text-gray-300">Username</label>
+            <div className="relative mt-2">
+              <AtSignIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 size-4.5"/>
+              <input type="text" placeholder="Choose a username" className="login-input" required/>
+            </div>
+          </div>
+        )}
+      </form>
+    </main>
+    </>
   )
 }
 
