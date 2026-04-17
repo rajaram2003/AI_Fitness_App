7bf285de03dffd3c7641e929ade300b6200f76c1
rajaram2003 <rajarambiswal123@gmail.com>
2026-04-17 19:47:59 +0530

    add

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -70,7 +70,8 @@ const Login = () => {
               placeholder="Enter your password" 
               className="login-input pr-10" required
               type={showPassword ? 'text' : 'password'}/>
-              <button type="button" className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600">
+              <button type="button" className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
+              onChange={()=> setShowPassword((p)=> !p)}>
                 {showPassword ? <EyeOffIcon size={16}/> : <EyeIcon size={16}/>}
               </button>
             </div>
