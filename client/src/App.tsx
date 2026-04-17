f489673856edeb019b6f90c116433316af79b8bd
rajaram2003 <rajarambiswal123@gmail.com>
2026-04-17 20:50:59 +0530

    add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,13 +7,19 @@ import Profile from "./pages/Profile"
 import { useAppContext } from "./context/AppContext"
 import Login from "./pages/Login"
 import Loading from "./components/Loading"
+import Onboarding from "./pages/Onboarding"
 
 const App = () => {
   const {user, isUserFetched, onboardingCompleted} = useAppContext()
 
   if(!user){
-    return isUserFetched ? <Login/> : <Loading /> 
+    return isUserFetched ? <Login /> : <Loading /> 
   }
+
+  if(!onboardingCompleted){
+    return <Onboarding />
+  }
+
   return (
     <>
       <Routes>
